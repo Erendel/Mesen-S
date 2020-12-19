92c3a3f1b97a28b5bd3303bfdc02e663a9e2265b
Vladimir Kononovich <vkononovich@ptsecurity.com>
2020-12-19 23:38:13 +0300

    Reformat UIWrapper (Resharper)

--- UIWrapper/UIWrapper.h
+++ UIWrapper/UIWrapper.h
@@ -4,33 +4,33 @@
 #include "../Core/Breakpoint.h"
 #include "../Core/INotificationListener.h"
 
-typedef void(__stdcall* NotificationListenerCallback)(::ConsoleNotificationType, void*);
+typedef void (__stdcall* NotificationListenerCallback)(::ConsoleNotificationType, void*);
 
 enum class ConsoleId
 {
-    Main = 0,
-    HistoryViewer = 1
+	Main = 0,
+	HistoryViewer = 1
 };
 
 extern "C" {
-    bool __stdcall IsMesenStarted();
+bool __stdcall IsMesenStarted();
 
-    void __stdcall Start(const char* rom);
-    void __stdcall Stop();
-    void __stdcall CloseGui();
+void __stdcall Start(const char* rom);
+void __stdcall Stop();
+void __stdcall CloseGui();
 
-    void __stdcall ResumeExecution();
-    unsigned int __stdcall GetPcAddress();
-    void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type);
-    void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes);
-    uint32_t __stdcall GetMemorySize(::SnesMemoryType type);
-    void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer);
-    void __stdcall GetState(::DebugState& state);
-    bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag);
-    void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value);
+void __stdcall ResumeExecution();
+unsigned int __stdcall GetPcAddress();
+void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type);
+void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes);
+uint32_t __stdcall GetMemorySize(::SnesMemoryType type);
+void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer);
+void __stdcall GetState(::DebugState& state);
+bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag);
+void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value);
 
-    void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback);
-    void __stdcall UnregisterNotificationCallback(void* notificationListener);
-    void __stdcall Release();
-    void __stdcall Pause(::ConsoleId consoleId);
+void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback);
+void __stdcall UnregisterNotificationCallback(void* notificationListener);
+void __stdcall Release();
+void __stdcall Pause(::ConsoleId consoleId);
 }
