f7849cdcc7620505eccca197651ca0d92199d231
Vladimir Kononovich <vkononovich@ptsecurity.com>
2020-12-19 23:10:11 +0300

    Added methods to stop emulation from IDA.

--- UIWrapper/UIWrapper.cpp
+++ UIWrapper/UIWrapper.cpp
@@ -6,93 +6,102 @@
 
 using namespace System::Runtime::InteropServices;
 using namespace System;
+using namespace System::Windows::Forms;
 using namespace msclr::interop;
 using namespace Mesen::GUI;
 
 extern "C" {
+    bool __stdcall IsMesenStarted() {
+        return EmuApi::IsRunning();
+    }
 
-	bool __stdcall IsMesenStarted() {
-		return EmuApi::IsRunning();
-	}
+    void __stdcall Start(const char* rom) {
+        CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
-	void __stdcall Start(const char* rom) {
-		CoInitializeEx(NULL, COINIT_MULTITHREADED);
-		//OleInitialize(NULL);
+        array<String^>^ arr = gcnew array<String^>(1);
 
-		array<String^>^ arr = gcnew array<String^>(1);
+        arr[0] = marshal_as<String^>(rom);
 
-		arr[0] = marshal_as<String^>(rom);
+        Program::Main(arr);
+    }
 
-		Program::Main(arr);
+    void __stdcall Stop() {
+        DebugApi::ResumeExecution();
 
-		//OleUninitialize();
-		CoUninitialize();
-	}
+        EmuApi::Stop();
+        EmuApi::Release();
+    }
 
-	void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type) {
-		DebugApi::Step((Mesen::GUI::CpuType)cpuType, instructionCount, (Mesen::GUI::StepType)type);
-	}
+    void __stdcall CloseGui() {
+        Application::Exit();
 
-	void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes) {
-		if (breakpoints != nullptr) {
-			array<Mesen::GUI::Debugger::Breakpoint^>^ bps = gcnew array<Mesen::GUI::Debugger::Breakpoint^>(execs + reads + writes);
-			DebugApi::GetBreakpoints((Mesen::GUI::CpuType)cpuType, bps, execs, reads, writes);
-			pin_ptr<Mesen::GUI::Debugger::Breakpoint^> bps_start = &bps[0];
-			memcpy(breakpoints, bps_start, execs + reads + writes);
-		}
-		else {
-			DebugApi::GetBreakpoints((Mesen::GUI::CpuType)cpuType, nullptr, execs, reads, writes);
-		}
-	}
+        CoUninitialize();
+    }
 
-	unsigned int __stdcall GetPcAddress() {
-		return DebugApi::GetPcAddress();
-	}
+    void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type) {
+        DebugApi::Step((Mesen::GUI::CpuType)cpuType, instructionCount, (Mesen::GUI::StepType)type);
+    }
 
-	void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback) {
-		auto ptr = Marshal::GetDelegateForFunctionPointer<NotificationListener::NotificationCallback^>(IntPtr(callback));
-		return (void*)EmuApi::RegisterNotificationCallback(ptr);
-	}
+    void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes) {
+        if (breakpoints != nullptr) {
+            array<Mesen::GUI::Debugger::Breakpoint^>^ bps = gcnew array<Mesen::GUI::Debugger::Breakpoint^>(execs + reads + writes);
+            DebugApi::GetBreakpoints((Mesen::GUI::CpuType)cpuType, bps, execs, reads, writes);
+            pin_ptr<Mesen::GUI::Debugger::Breakpoint^> bps_start = &bps[0];
+            memcpy(breakpoints, bps_start, execs + reads + writes);
+        }
+        else {
+            DebugApi::GetBreakpoints((Mesen::GUI::CpuType)cpuType, nullptr, execs, reads, writes);
+        }
+    }
 
-	void __stdcall UnregisterNotificationCallback(void* notificationListener) {
-		EmuApi::UnregisterNotificationCallback(IntPtr(notificationListener));
-	}
+    unsigned int __stdcall GetPcAddress() {
+        return DebugApi::GetPcAddress();
+    }
 
-	uint32_t __stdcall GetMemorySize(::SnesMemoryType type) {
-		return DebugApi::GetMemorySize((Mesen::GUI::SnesMemoryType)type);
-	}
+    void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback) {
+        auto ptr = Marshal::GetDelegateForFunctionPointer<NotificationListener::NotificationCallback^>(IntPtr(callback));
+        return (void*)EmuApi::RegisterNotificationCallback(ptr);
+    }
 
-	void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer) {
-		auto size = GetMemorySize(type);
-		auto arr = gcnew array<unsigned char>(size);
+    void __stdcall UnregisterNotificationCallback(void* notificationListener) {
+        EmuApi::UnregisterNotificationCallback(IntPtr(notificationListener));
+    }
 
-		DebugApi::GetMemoryStateWrapper((Mesen::GUI::SnesMemoryType)type, arr);
+    uint32_t __stdcall GetMemorySize(::SnesMemoryType type) {
+        return DebugApi::GetMemorySize((Mesen::GUI::SnesMemoryType)type);
+    }
 
-		pin_ptr<unsigned char> arr_start = &arr[0];
-		memcpy(buffer, arr_start, size);
-	}
+    void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer) {
+        auto size = GetMemorySize(type);
+        auto arr = gcnew array<unsigned char>(size);
 
-	void __stdcall GetState(::DebugState& state) {
-		DebugApi::GetStateWrapper((Mesen::GUI::DebugState%)state);
-	}
+        DebugApi::GetMemoryStateWrapper((Mesen::GUI::SnesMemoryType)type, arr);
 
-	bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag) {
-		return DebugApi::GetCpuProcFlag((Mesen::GUI::ProcFlags)flag);
-	}
+        pin_ptr<unsigned char> arr_start = &arr[0];
+        memcpy(buffer, arr_start, size);
+    }
 
-	void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value) {
-		DebugApi::SetCpuRegister((Mesen::GUI::CpuRegister)reg, value);
-	}
+    void __stdcall GetState(::DebugState& state) {
+        DebugApi::GetStateWrapper((Mesen::GUI::DebugState%)state);
+    }
 
-	void __stdcall Release() {
-		EmuApi::Release();
-	}
+    bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag) {
+        return DebugApi::GetCpuProcFlag((Mesen::GUI::ProcFlags)flag);
+    }
 
-	void __stdcall ResumeExecution() {
-		DebugApi::ResumeExecution();
-	}
+    void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value) {
+        DebugApi::SetCpuRegister((Mesen::GUI::CpuRegister)reg, value);
+    }
 
-	void __stdcall Pause(::ConsoleId consoleId) {
-		EmuApi::Pause((Mesen::GUI::EmuApi::ConsoleId)consoleId);
-	}
+    void __stdcall Release() {
+        EmuApi::Release();
+    }
+
+    void __stdcall ResumeExecution() {
+        DebugApi::ResumeExecution();
+    }
+
+    void __stdcall Pause(::ConsoleId consoleId) {
+        EmuApi::Pause((Mesen::GUI::EmuApi::ConsoleId)consoleId);
+    }
 }
--- UIWrapper/UIWrapper.h
+++ UIWrapper/UIWrapper.h
@@ -4,45 +4,33 @@
 #include "../Core/Breakpoint.h"
 #include "../Core/INotificationListener.h"
 
-#ifdef STATIC_API
-#    define LIBRARY_API
-#else
-	#ifdef LIBRARY_EXPORTS
-	#    define LIBRARY_API __declspec(dllexport)
-	#else
-	#    define LIBRARY_API __declspec(dllimport)
-	#endif
-#endif
-
-#    define LIBRARY_API
-
-
 typedef void(__stdcall* NotificationListenerCallback)(::ConsoleNotificationType, void*);
 
 enum class ConsoleId
 {
-	Main = 0,
-	HistoryViewer = 1
+    Main = 0,
+    HistoryViewer = 1
 };
 
 extern "C" {
+    bool __stdcall IsMesenStarted();
 
-	LIBRARY_API bool __stdcall IsMesenStarted();
+    void __stdcall Start(const char* rom);
+    void __stdcall Stop();
+    void __stdcall CloseGui();
 
-	LIBRARY_API void __stdcall Start(const char* rom);
-	LIBRARY_API void __stdcall ResumeExecution();
-	LIBRARY_API unsigned int __stdcall GetPcAddress();
-	LIBRARY_API void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type);
-	LIBRARY_API void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes);
-	LIBRARY_API uint32_t __stdcall GetMemorySize(::SnesMemoryType type);
-	LIBRARY_API void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer);
-	LIBRARY_API void __stdcall GetState(::DebugState& state);
-	LIBRARY_API bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag);
-	LIBRARY_API void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value);
+    void __stdcall ResumeExecution();
+    unsigned int __stdcall GetPcAddress();
+    void __stdcall Step(::CpuType cpuType, int instructionCount, ::StepType type);
+    void __stdcall GetBreakpoints(::CpuType cpuType, ::Breakpoint* breakpoints, int& execs, int& reads, int& writes);
+    uint32_t __stdcall GetMemorySize(::SnesMemoryType type);
+    void __stdcall GetMemoryState(::SnesMemoryType type, uint8_t* buffer);
+    void __stdcall GetState(::DebugState& state);
+    bool __stdcall GetCpuProcFlag(::ProcFlags::ProcFlags flag);
+    void __stdcall SetCpuRegister(::CpuRegister reg, uint16_t value);
 
-	LIBRARY_API void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback);
-	LIBRARY_API void __stdcall UnregisterNotificationCallback(void* notificationListener);
-	LIBRARY_API void __stdcall Release();
-	LIBRARY_API void __stdcall Pause(::ConsoleId consoleId);
-
+    void* __stdcall RegisterNotificationCallback(NotificationListenerCallback callback);
+    void __stdcall UnregisterNotificationCallback(void* notificationListener);
+    void __stdcall Release();
+    void __stdcall Pause(::ConsoleId consoleId);
 }
